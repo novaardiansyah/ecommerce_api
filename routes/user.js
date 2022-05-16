95b74f4853e87288c16080da19c48be2f8863d90
novaardiansyah <novaardiansyah78@gmail.com>
2022-05-16 11:15:17 +0700

    Generate tokens when logging in, and verify tokens when processing something

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
+const { verifyTokenAndVerification } = require('../routes/jwt');
+
+// * Models (Start)
+const User = require('../models/users');
+// * Models (End)
 
 router.get('/', (req, res) => {
   res.send('Hello World!');
@@ -7,6 +12,24 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   let username = req.body.username;
   res.send('Hello ' + username);
+});
+
+router.put('/:_id', verifyTokenAndVerification, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_PREFIX).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params._id, {
+      $set: req.body,
+    }, { new: true });
+
+    const { password, ...others } = updatedUser.toObject();
+
+    res.status(200).json({ status: true, message: 'User updated successfully', data: others });
+  } catch (error) {
+    res.status(400).json({ status: false, message: 'User not updated', error });
+  }
 });
 
 module.exports = router;
